4a25846707582bd4e631fc58536e7bf4e60fce4a
chicken8848 <mech_4e@hotmail.com>
2025-09-09 23:52:23 +0800

    [FEAT] Add range and convert to universal argument

--- pdfsplit.sh
+++ pdfsplit.sh
@@ -1,10 +1,23 @@
 #!/usr/bin/env zsh
 
-while getopts "f:l:" opt; do
+expand_pages() {
+    local pages=()
+    for token in ${(s: :)1}; do
+        if [[ $token == *-* ]]; then
+            local start=${token%-*}
+            local end=${token#*-}
+            pages+=($(seq $start $end))
+        else
+            pages+=($token)
+        fi
+    done
+    echo $pages
+}
+
+while getopts "p:" opt; do
     case $opt in 
-        f) first_page=$OPTARG ;;
-        l) last_page=$OPTARG ;;
-        \?) echo "Usage: $0 -f first_page -l last_page input.pdf output.pdf" >&2; exit 1 ;;
+        p) pages_string=$OPTARG ;;
+        \?) echo "Usage: $0 -p '1-2 3 4 5-11' input.pdf output.pdf" >&2; exit 1 ;;
     esac
 done
 
@@ -12,7 +25,8 @@ shift $((OPTIND - 1))
 
 input_file=$1
 output_file=$2
+pages=($(expand_pages "$pages_string"))
 
 pdfseparate "$input_file" _out%d.pdf
-pdfunite $(seq $first_page $last_page | xargs -I{} printf "_out%d.pdf " {}) "$output_file"
+pdfunite $(for p in $pages; do printf "_out%d.pdf " $p; done) "$output_file"
 rm -f _out*.pdf
